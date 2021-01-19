68707c9f62a3c6c52aa03a606bbb15195f5ec37a
User <User@DESKTOP-UTIESKD>
2021-01-19 14:11:39 +0700

    first

---
+++ Record_CameraIP/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Record_CameraIP
+{
+     partial class Form1
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.txtIP_Address = new System.Windows.Forms.TextBox();
+               this.label1 = new System.Windows.Forms.Label();
+               this.groupBox1 = new System.Windows.Forms.GroupBox();
+               this.btnOpen = new System.Windows.Forms.Button();
+               this.btnRecord = new System.Windows.Forms.Button();
+               this.btnStop = new System.Windows.Forms.Button();
+               this.btnStart = new System.Windows.Forms.Button();
+               this.label2 = new System.Windows.Forms.Label();
+               this.panel1 = new System.Windows.Forms.Panel();
+               this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+               this.groupBox1.SuspendLayout();
+               this.SuspendLayout();
+               // 
+               // txtIP_Address
+               // 
+               this.txtIP_Address.Location = new System.Drawing.Point(76, 362);
+               this.txtIP_Address.Name = "txtIP_Address";
+               this.txtIP_Address.Size = new System.Drawing.Size(502, 20);
+               this.txtIP_Address.TabIndex = 1;
+               // 
+               // label1
+               // 
+               this.label1.AutoSize = true;
+               this.label1.Location = new System.Drawing.Point(12, 365);
+               this.label1.Name = "label1";
+               this.label1.Size = new System.Drawing.Size(58, 13);
+               this.label1.TabIndex = 2;
+               this.label1.Text = "IP Address";
+               // 
+               // groupBox1
+               // 
+               this.groupBox1.Controls.Add(this.btnOpen);
+               this.groupBox1.Controls.Add(this.btnRecord);
+               this.groupBox1.Controls.Add(this.btnStop);
+               this.groupBox1.Controls.Add(this.btnStart);
+               this.groupBox1.Location = new System.Drawing.Point(15, 398);
+               this.groupBox1.Name = "groupBox1";
+               this.groupBox1.Size = new System.Drawing.Size(563, 70);
+               this.groupBox1.TabIndex = 3;
+               this.groupBox1.TabStop = false;
+               this.groupBox1.Text = "Controls";
+               // 
+               // btnOpen
+               // 
+               this.btnOpen.Location = new System.Drawing.Point(482, 31);
+               this.btnOpen.Name = "btnOpen";
+               this.btnOpen.Size = new System.Drawing.Size(75, 23);
+               this.btnOpen.TabIndex = 10;
+               this.btnOpen.Text = "Open";
+               this.btnOpen.UseVisualStyleBackColor = true;
+               // 
+               // btnRecord
+               // 
+               this.btnRecord.Location = new System.Drawing.Point(326, 31);
+               this.btnRecord.Name = "btnRecord";
+               this.btnRecord.Size = new System.Drawing.Size(75, 23);
+               this.btnRecord.TabIndex = 9;
+               this.btnRecord.Text = "Record";
+               this.btnRecord.UseVisualStyleBackColor = true;
+               this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+               // 
+               // btnStop
+               // 
+               this.btnStop.Location = new System.Drawing.Point(161, 31);
+               this.btnStop.Name = "btnStop";
+               this.btnStop.Size = new System.Drawing.Size(75, 23);
+               this.btnStop.TabIndex = 8;
+               this.btnStop.Text = "Stop";
+               this.btnStop.UseVisualStyleBackColor = true;
+               this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+               // 
+               // btnStart
+               // 
+               this.btnStart.Location = new System.Drawing.Point(6, 31);
+               this.btnStart.Name = "btnStart";
+               this.btnStart.Size = new System.Drawing.Size(75, 23);
+               this.btnStart.TabIndex = 7;
+               this.btnStart.Text = "Start";
+               this.btnStart.UseVisualStyleBackColor = true;
+               this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+               // 
+               // label2
+               // 
+               this.label2.AutoSize = true;
+               this.label2.Location = new System.Drawing.Point(271, 9);
+               this.label2.Name = "label2";
+               this.label2.Size = new System.Drawing.Size(41, 13);
+               this.label2.TabIndex = 4;
+               this.label2.Text = "Display";
+               // 
+               // panel1
+               // 
+               this.panel1.Location = new System.Drawing.Point(15, 34);
+               this.panel1.Name = "panel1";
+               this.panel1.Size = new System.Drawing.Size(563, 310);
+               this.panel1.TabIndex = 8;
+               // 
+               // Form1
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(593, 486);
+               this.Controls.Add(this.panel1);
+               this.Controls.Add(this.label2);
+               this.Controls.Add(this.groupBox1);
+               this.Controls.Add(this.label1);
+               this.Controls.Add(this.txtIP_Address);
+               this.Name = "Form1";
+               this.Text = "Form1";
+               this.groupBox1.ResumeLayout(false);
+               this.ResumeLayout(false);
+               this.PerformLayout();
+
+          }
+
+          #endregion
+          private System.Windows.Forms.TextBox txtIP_Address;
+          private System.Windows.Forms.Label label1;
+          private System.Windows.Forms.GroupBox groupBox1;
+          private System.Windows.Forms.Button btnOpen;
+          private System.Windows.Forms.Button btnRecord;
+          private System.Windows.Forms.Button btnStop;
+          private System.Windows.Forms.Button btnStart;
+          private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+     }
+}
+
---
+++ Record_CameraIP/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using Ozeki.Media;
+using Ozeki.Camera;
+using Ozeki.VoIP;
+using Accord.Video.FFMPEG;
+
+namespace Record_CameraIP
+{
+     public partial class Form1 : Form
+     {
+        //MJPEGStream stream;
+        private IIPCamera _camera;
+        private DrawingImageProvider _imageProvider = new DrawingImageProvider();
+        private MediaConnector _connector = new MediaConnector();
+        private VideoViewerWF _videoViewerWF1;
+        public VideoFileWriter FileWriter = new VideoFileWriter();
+        private MPEG4Recorder _mpeg4Recorder;
+        public Form1()
+          {
+               InitializeComponent();
+            //stream = new MJPEGStream("rtsp://khanh29bk:Admin123@192.168.10.12/Src/MediaInput/h264/stream_1/ch_");
+            //stream.NewFrame += GetNewFrame;
+            // Create video viewer UI control
+            
+        }
+
+          //void GetNewFrame(object sender, NewFrameEventArgs eventarg)
+          //{
+          //     Bitmap bmp = (Bitmap)eventarg.Frame.Clone();
+          //     display.Image = bmp;
+          //}
+
+          private void btnStart_Click(object sender, EventArgs e)
+          {
+            panel1.Controls.Clear();
+            _videoViewerWF1 = new VideoViewerWF();
+            _videoViewerWF1.Name = "videoViewerWF1";
+            _videoViewerWF1.Size = panel1.Size;
+
+
+            // Bind the camera image to the UI control
+            _videoViewerWF1.SetImageProvider(_imageProvider);
+            panel1.Controls.Add(_videoViewerWF1);
+
+            //stream.Start();
+            _camera = IPCameraFactory.GetCamera("rtsp://sla:1123456@117.6.121.13:554/axis-media/media.amp", "sla", "1123456");
+            _connector.Connect(_camera.VideoChannel, _imageProvider);
+            _camera.Start();
+            _videoViewerWF1.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(btnStop.Text == "Stop")
+            {
+                _videoViewerWF1.Stop();
+            }
+
+            if(btnStop.Text == "Stop Record")
+            {
+                StopVideoRecord();
+                btnRecord.Text = "Record";
+                btnRecord.BackColor = Color.FromKnownColor(KnownColor.Control);
+                btnStop.Text = "Stop";
+
+                
+
+            }
+            
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            if(btnStop.Text == "Stop")
+            {
+                btnStop.Text = "Stop Record";
+                btnRecord.Text = "Recording";
+                btnRecord.BackColor = Color.Red;
+                
+                // Lấy ổ cứng muốn lưu từ file config
+                FileStream fs = new FileStream(@"E:\record_CameraIP\Record_CameraIP\config.txt", FileMode.Open);
+                StreamReader rd = new StreamReader(fs, Encoding.UTF8);
+                string source = rd.ReadLine();
+
+                var date = DateTime.Now.Year + "y-" + DateTime.Now.Month + "m-" + DateTime.Now.Day + "d-";
+                
+                string path = source + date;
+                if(Directory.Exists(path))
+                         StartVideoRecord(path);
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                StartVideoRecord(path);
+            }
+            
+
+        }
+
+        private void StartVideoRecord(string path)
+        {
+            var date = DateTime.Now.Hour + "h-" + DateTime.Now.Minute + "m-" + DateTime.Now.Second + "s";
+            string currentpath;
+            if (String.IsNullOrEmpty(path))
+                currentpath = date + ".mp4";
+            else
+                currentpath = path + "\\" + date + ".mp4";
+
+            _mpeg4Recorder = new MPEG4Recorder(currentpath);
+            _mpeg4Recorder.MultiplexFinished += Mpeg4Recorder_MultiplexFinished;
+            //_connector.Connect(_camera.AudioChannel, _mpeg4Recorder.AudioRecorder);
+            _connector.Connect(_camera.VideoChannel, _mpeg4Recorder.VideoRecorder);
+        }
+
+        private void Mpeg4Recorder_MultiplexFinished(object sender, VoIPEventArgs<bool> e)
+        {
+            _mpeg4Recorder.MultiplexFinished -= Mpeg4Recorder_MultiplexFinished;
+            //_connector.Disconnect(_camera.AudioChannel, _mpeg4Recorder.AudioRecorder);
+            //_connector.Disconnect(_camera.VideoChannel, _mpeg4Recorder.VideoRecorder);
+            _mpeg4Recorder.Dispose();
+        }
+
+        
+        private void StopVideoRecord()
+        {
+            if (_mpeg4Recorder != null)
+            {
+                _mpeg4Recorder.Multiplex();
+                //_connector.Disconnect(_camera.AudioChannel, _mpeg4Recorder.AudioRecorder);
+                _connector.Disconnect(_camera.VideoChannel, _mpeg4Recorder.VideoRecorder);
+            }
+        }
+
+     }
+}
